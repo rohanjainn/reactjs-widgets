9db4daff0f77cd462439ee0a201daa31eadfcbba
rohan <iamrohanjainn@gmail.com>
2020-11-25 14:01:52 +0530

    accordion changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import React from 'react';
+import Accordion from './components/Accordion'
+
+const items=[
+{
+    title:'What is react?',
+    content:'Front end JS lib'
+},
+{
+    title:'why react?',
+    content:`it's cool`
+},
+{
+    title:'How do you use React',
+    content:'createing components'
+}
+];
 
 export default()=>{
 
-    return <h1>widgets app</h1>
+    return (
+       <div>
+            <h1>Widget app</h1>
+            <br/>
+        <Accordion items={items}/> 
+        
+        </div>
+        )
 };
